ce3c455357b6598f424967db05040a190d8a8862
umarnazarov <83394723+umarnazarov@users.noreply.github.com>
2022-07-29 21:49:39 +0500

    multiple pointers

---
+++ Multiple Pointers/index.js
@@ -0,0 +1,28 @@
+function sumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === 0) {
+            return [arr[left], arr[right]]
+        } else if (sum > 0) {
+            right--
+        } else {
+            left++
+        }
+    }
+}
+
+//Unique Values
+
+function uniqueValues(arr) {
+    let i = 0;
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1
+}
