855416d14f7fe3f946b66ea2b1e7fed1405120bd
umarnazarov <83394723+umarnazarov@users.noreply.github.com>
2022-07-29 21:49:11 +0500

    Binary search

---
+++ BinarySearch/index.js
@@ -0,0 +1,13 @@
+function binarySearch(arr, n) {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((start + end) / 2);
+
+    while (arr[middle] !== n && start <= end) {
+        if (arr[middle] < n) start = middle + 1;
+        else end = middle - 1;
+        middle = Math.floor((start + end) / 2);
+    }
+
+    return arr[middle] === n ? middle : -1
+}
