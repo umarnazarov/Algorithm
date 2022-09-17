80342fe6351a927157620e4ea63d075c34fd199a
Umar Nazarov <1nazarovumar@gmail.com>
2022-09-17 16:17:29 -0700

    upd

--- BinarySearch/index.js
+++ BinarySearch/index.js
@@ -1,6 +1,7 @@
 function binarySearch(arr, n) {
     let start = 0;
     let end = arr.length - 1;
+    
     let middle = Math.floor((start + end) / 2);
 
     while (arr[middle] !== n && start <= end) {
